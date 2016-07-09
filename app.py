a2021a4d8466aa9aedae13582e22817128794449
c-andrews <chris@c-andrews.co.uk>
2016-07-09 11:56:40 +0100

    updated switch int

--- app.py
+++ app.py
@@ -206,18 +206,18 @@ class relaySwitch:
 		for x in range( 0, 45 ):
 			
 			# if i is 0 then trigger the switch
-			if i == 0 :
+			if i == 1 :
 				self.switch( x )
 			
 				# Wait for the sleep time
 				time.sleep( self.sleepTime + 0.1 );
 
 				# Increase I so that we miss the open relay
-				i+=1
+				i=0
 
 			# if i is not 0 then reset it to 0
 			else :
-				i=0
+				i+=1
 
 		
 
