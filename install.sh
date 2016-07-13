de1052d0ac7c19bde489a1a7c9c1d14a8d3b0fe6
c-andrews <chris@c-andrews.co.uk>
2016-07-13 16:05:12 +0100

    Updated shell script

--- install.sh
+++ install.sh
@@ -2,18 +2,20 @@
 
 path = pwd
 
+echo "Current Dir: "+ pwd
+
 # Make the www directory if it doesnt exsist
 echo "Making /var/www/ directory"
 mkdir -p "/var/www/"
 
 # Copy the app.py file to /home/pi/
-echo "Copying ${path}/app.py to /home/pi/"
-cp "${path}/app.py" "/home/pi/"
+echo "Copying app.py to /home/pi/"
+cp "app.py" "/home/pi/"
 
 # Copy the app.html to /var/www/
-echo "Copying ${path}/app.html to /var/www/"
-cp "${path}/app.html" "/var/www/"
+echo "Copying app.html to /var/www/"
+cp "app.html" "/var/www/"
 
 # Copy the assets folder to /var/www/
-echo "Copying ${path}/assets to /var/www/"
-cp -avr "${path}/assets" "/var/www/"
+echo "Copying assets to /var/www/"
+cp -avr "assets" "/var/www/"
